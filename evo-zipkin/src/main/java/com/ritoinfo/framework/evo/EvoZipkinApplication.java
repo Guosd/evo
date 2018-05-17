bd5c7a8f064436635978d0605098cd3c7078bae8
kyllyang <kyllyang@gmail.com>
2018-05-17 08:52:28 +0800

    修改zipkin服务配置

--- evo-zipkin/src/main/java/com/ritoinfo/framework/evo/EvoZipkinApplication.java
+++ evo-zipkin/src/main/java/com/ritoinfo/framework/evo/EvoZipkinApplication.java
@@ -2,11 +2,13 @@ package com.ritoinfo.framework.evo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
 /**
  * User: Kyll
  * Date: 2018-02-26 20:53
  */
+@EnableZipkinServer
 @SpringBootApplication
 public class EvoZipkinApplication {
 	public static void main(String[] args) {
