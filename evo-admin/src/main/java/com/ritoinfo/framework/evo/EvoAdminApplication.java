24f8cfbcd2ca43a2d6e66ccc6a66cc9a71ed877b
kyllyang <kyllyang@gmail.com>
2018-05-25 11:17:46 +0800

    修改admin配置

--- evo-admin/src/main/java/com/ritoinfo/framework/evo/EvoAdminApplication.java
+++ evo-admin/src/main/java/com/ritoinfo/framework/evo/EvoAdminApplication.java
@@ -2,16 +2,16 @@ package com.ritoinfo.framework.evo;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * User: Kyll
  * Date: 2018-02-26 20:53
  */
-@EnableDiscoveryClient
+@Configuration
+@EnableAutoConfiguration
 @EnableAdminServer
-@SpringBootApplication
 public class EvoAdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EvoAdminApplication.class, args);
