6c451c705971b9ccdfc6b97e4c14c2b54cbbe9d5
kyllyang <kyllyang@gmail.com>
2019-05-23 17:38:24 +0800

    加入swagger2注解

--- evo-services/evo-controller/src/main/java/com/github/framework/evo/EvoControllerApplication.java
+++ evo-services/evo-controller/src/main/java/com/github/framework/evo/EvoControllerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * User: Kyll
  * Date: 2017-11-27 11:57
  */
+@EnableSwagger2
 @EnableFeignClients(basePackages = "com.github.framework.evo")
 @EnableCircuitBreaker
 @SpringBootApplication
