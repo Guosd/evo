485402fcbf5ac8f6d39992f77e35540dbd51490c
kyllyang <kyllyang@gmail.com>
2019-06-18 14:17:46 +0800

    修改跨域写法

--- evo-autoconfigure/src/main/java/com/github/framework/evo/autoconfigure/controller/ControllerConfiguration.java
+++ evo-autoconfigure/src/main/java/com/github/framework/evo/autoconfigure/controller/ControllerConfiguration.java
@@ -3,10 +3,14 @@ package com.github.framework.evo.autoconfigure.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -43,5 +47,22 @@ public class ControllerConfiguration {
 						.exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(18000L);
 			}
 		};
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.setMaxAge(18000L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 }
