97c0f3762d3fc39cdb07d745008f7a8e1051b2b3
kyllyang <kyllyang@gmail.com>
2019-06-18 11:05:24 +0800

    增加跨域配置

--- evo-autoconfigure/src/main/java/com/github/framework/evo/autoconfigure/controller/ControllerConfiguration.java
+++ evo-autoconfigure/src/main/java/com/github/framework/evo/autoconfigure/controller/ControllerConfiguration.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * User: Kyll
@@ -25,5 +28,20 @@ public class ControllerConfiguration {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebMvcConfigurationSupport getConfigurer() {
+		return new WebMvcConfigurationSupport() {
+			@Override
+			protected void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true)
+						.exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(18000L);
+			}
+		};
 	}
 }
